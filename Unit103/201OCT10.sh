51fef921501cf11b558e1f2e4555a8807824fde7
Johnthan Butler <johnthanb27@gmail.com>
2023-11-06 22:22:18 -0600

    201OCT.sh

--- Unit103/201OCT10.sh
+++ Unit103/201OCT10.sh
@@ -1,13 +1,8 @@
-#!/BIN/BASH
+#!/bin/bash
 
 # Script: Ops 201 Class 01 Ops Challenge Solution
 # Author: Johnthan Butler
 # Date of latest revision: 11/6/23
 # purpose: Print a string to the terminal
 
-echo "enter a website"
-read website
-whois $website >> whois.txt
-dig $website >> whois.txt
-host $website >> whois.txt
-nslookup >> whois.txt
+echo "hello world"
