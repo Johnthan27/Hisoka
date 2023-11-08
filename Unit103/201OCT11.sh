d963ee378b91bd1799a729d5a302c438bb284fc9
Johnthan Butler <johnthanb27@gmail.com>
2023-11-07 21:39:34 -0600

    201OCT11.sh

---
+++ Unit103/201OCT11.sh
@@ -0,0 +1,12 @@
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+echo "what is your name"
+
+read name
+
+echo "Welcome to the class $name"
+sleep 1
+echo "how is your day going $name"
+sleep 1
+echo "i hope you learn something new today"
